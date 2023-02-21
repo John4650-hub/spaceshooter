111b6e94c92bd62b04fe34251640868104023e5b
john4650-hub <johndelvin51@gmail.com>
2023-02-21 05:05:48 +0300

    fix error

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
   mode:'production',
-  entry: './www/imdex.js',
+  entry: './www/index.js',
   output: {
     filename: 'app.bundle.js',
     path: path.join(__dirname,'www/dist/')
